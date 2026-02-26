a8d934f4d1336e17a4293611b18009f5305c9eaf
titus-t <titus.torvelainen@myy.haaga-helia.fi>
2026-02-26 16:00:16 +0200

    First commit

---
+++ src/main/java/backend/gymtracker/GymtrackerApplication.java
@@ -0,0 +1,13 @@
+package backend.gymtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymtrackerApplication.class, args);
+	}
+
+}
